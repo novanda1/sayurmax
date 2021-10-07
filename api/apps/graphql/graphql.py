37e5a19ca8d95d07ae7670dd0c5f8d56b96252f3
Novanda Ahsan <nathoilla@indo-soft.com>
2021-10-07 15:33:46 +0700

    initilize backend

---
+++ api/apps/graphql/graphql.py
@@ -0,0 +1,11 @@
+import strawberry
+
+from apps.graphql.schema.hello import say_hello
+
+
+@strawberry.type
+class Query:
+    hello: str = strawberry.field(resolver=say_hello)
+
+
+schema = strawberry.Schema(query=Query)
