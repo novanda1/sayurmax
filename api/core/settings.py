459aaf05fb79ef95b654a8e9c575397b58ee3a2e
Novanda Ahsan <novandaahsan1@gmail.com>
2021-10-08 13:26:51 +0700

    api: cors allow all

--- api/core/settings.py
+++ api/core/settings.py
@@ -48,13 +48,15 @@ INSTALLED_APPS = [
     'apps.grocery',
     'apps.user',
 
-    # 3rd app
+    # deps
     'phonenumber_field',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -135,3 +137,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS=True
