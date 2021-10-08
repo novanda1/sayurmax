cb1fe5681e4ee9b8098852dc02f199cc33108730
Novanda Ahsan <novandaahsan1@gmail.com>
2021-10-08 14:28:34 +0700

    api: user: add password

--- api/apps/user/admin.py
+++ api/apps/user/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from apps.user.models import User
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('display_name', 'username', 'email',
+                    'phone')
+    fields = ('display_name', 'username', 'email',
+              'phone')
--- api/apps/user/models.py
+++ api/apps/user/models.py
@@ -9,6 +9,7 @@ class User(models.Model):
     username = models.CharField(max_length=100, unique=True)
     display_name = models.CharField(max_length=100)
     phone = PhoneNumberField(blank=True, default=None, unique=True)
+    password = models.CharField(max_length=254, default="not set")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
