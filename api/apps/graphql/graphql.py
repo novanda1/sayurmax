7f3015a1f9ef4999d0cc74c32b3756a6e64d90c5
Novanda Ahsan <novandaahsan1@gmail.com>
2021-10-11 21:24:08 +0700

    api: add user query

--- api/apps/graphql/graphql.py
+++ api/apps/graphql/graphql.py
@@ -2,7 +2,7 @@ import strawberry
 from typing import List
 
 from apps.graphql.queries.hello import say_hello
-from apps.graphql.queries.user import get_users
+from apps.graphql.queries.user import get_users, get_user
 from apps.graphql.mutations.user import register, login
 from apps.graphql.schema.user import UserType, UserResponse
 
@@ -11,6 +11,7 @@ from apps.graphql.schema.user import UserType, UserResponse
 class Query:
     hello: str = strawberry.field(resolver=say_hello)
     users: List[UserType] = strawberry.field(resolver=get_users)
+    user: UserType = strawberry.field(resolver=get_user)
 
 
 @strawberry.type
--- api/apps/graphql/queries/user.py
+++ api/apps/graphql/queries/user.py
@@ -4,3 +4,7 @@ from apps.user.models import User
 def get_users():
     users = User.objects.all()
     return users
+
+def get_user(pk: int):
+    user = User.objects.get(pk=pk)
+    return user
