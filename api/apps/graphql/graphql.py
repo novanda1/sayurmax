c9621b44ff1877ad22ef2ffed8d49198b8789f77
Novanda Ahsan <novandaahsan1@gmail.com>
2021-10-13 14:03:59 +0700

    api: verify jwt query

--- api/apps/graphql/graphql.py
+++ api/apps/graphql/graphql.py
@@ -2,7 +2,7 @@ import strawberry
 from typing import List
 
 from apps.graphql.queries.hello import say_hello
-from apps.graphql.queries.user import get_users, get_user
+from apps.graphql.queries.user import get_users, get_user, verify_jwt
 from apps.graphql.mutations.user import register, login
 from apps.graphql.schema.user import UserType, UserResponse
 
@@ -12,6 +12,7 @@ class Query:
     hello: str = strawberry.field(resolver=say_hello)
     users: List[UserType] = strawberry.field(resolver=get_users)
     user: UserType = strawberry.field(resolver=get_user)
+    verify_jwt: bool = strawberry.field(resolver=verify_jwt)
 
 
 @strawberry.type
--- api/apps/graphql/queries/user.py
+++ api/apps/graphql/queries/user.py
@@ -1,10 +1,24 @@
 from apps.user.models import User
+import jwt
 
 
 def get_users():
     users = User.objects.all()
     return users
 
+
 def get_user(pk: int):
     user = User.objects.get(pk=pk)
     return user
+
+
+def verify_jwt(token: str):
+    valid = jwt.decode(
+        token,
+        os.getenv("JWT_SECRET")
+    )
+
+    if valid:
+        return True
+    else:
+        return False
