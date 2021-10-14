852c5ee1e468f800dfc0c29a88e6c1e330e87cdf
Novanda Ahsan <novandaahsan1@gmail.com>
2021-10-14 12:41:51 +0700

    completely setup product model

--- api/apps/grocery/models.py
+++ api/apps/grocery/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
+from djmoney.models.fields import MoneyField
+from django.utils.translation import gettext as _
 
 
 class Category(models.Model):
@@ -18,10 +20,32 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
 
+item_choices = [
+    "pack",
+    "gram",
+    "kg",
+    "pcs"
+]
+
+ITEM_UNIT_CHOICES = tuple((str(t), str(t)) for t in item_choices)
+
+# for it in ITEM_UNIT_CHOICES:
+#     ITEM_UNIT_CHOICES.append(tuple( zip(it, it)))
+
+
 class Product(models.Model):
     title = models.CharField(max_length=250,)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     categories = models.ManyToManyField(Category)
+    image_url = models.CharField(
+        _("Image Url"), max_length=250,)
+    normal_price = models.BigIntegerField()
+    dicount_price = models.BigIntegerField(
+      null=True, blank=True, default=None)
+    item_unit = models.CharField(max_length=10, choices=ITEM_UNIT_CHOICES)
+    information = models.TextField(_("Product Infromation"))
+    nutrition = models.TextField(_("Product Nutrition"))
+    how_to_keep = models.TextField(_("How To Keep"))
 
     def __str__(self):
         return self.title
