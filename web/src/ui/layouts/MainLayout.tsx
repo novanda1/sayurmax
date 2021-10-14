5783538f21e9f69207c1ad256c2afb123daee224
Novanda Ahsan <novandaahsan1@gmail.com>
2021-10-14 16:23:46 +0700

    web: returning child

--- web/src/ui/layouts/MainLayout.tsx
+++ web/src/ui/layouts/MainLayout.tsx
@@ -5,10 +5,13 @@ import { Nabvar } from "../components/Nabvar";
 
 export const MainLayout: React.FC<typeof RootStateContextValue> = ({
     userStore,
+    children,
 }) => {
     return (
         <Box minH="100vh" bgColor="gray.100">
             <Nabvar user={userStore.currentUser ?? null} />
+
+            {children}
         </Box>
     );
 };
