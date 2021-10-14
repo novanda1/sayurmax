a7c2ee28dd129c8f27800fea4c2c4a7ee6f04e8e
Novanda Ahsan <novandaahsan1@gmail.com>
2021-10-14 07:33:47 +0700

    unknown progress

--- api/apps/grocery/admin.py
+++ api/apps/grocery/admin.py
@@ -1,3 +1,14 @@
+from apps.grocery.model import category, product
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(category.Category)
+class UserAdmin(admin.ModelAdmin):
+    exclude=("slug",)
+
+@admin.register(product.Product)
+class UserAdmin(admin.ModelAdmin):
+    exclude=("slug",)
+    list_display = ('title', )
+    fields = ('title','categories')
+
