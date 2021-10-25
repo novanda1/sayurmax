444d8e63e2eacb45a9a45c64bae2805d81b43686
Novanda Ahsan <novandaahsan1@gmail.com>
2021-10-25 07:05:22 +0700

    edit user schema mark id as str

--- api/apps/graphql/queries/user.py
+++ api/apps/graphql/queries/user.py
@@ -2,12 +2,23 @@ from apps.user.models import User
 import jwt
 
 
+from dotenv import load_dotenv, dotenv_values
+import os
+
+config = {
+    **dotenv_values(".env.dev"),
+    **os.environ,
+}
+
+load_dotenv()
+
+
 def get_users():
     users = User.objects.all()
     return users
 
 
-def get_user(pk: int):
+def get_user(pk: str):
     user = User.objects.get(pk=pk)
     return user
 
@@ -15,7 +26,7 @@ def get_user(pk: int):
 def verify_jwt(token: str):
     valid = jwt.decode(
         token,
-        os.getenv("JWT_SECRET")
+        config['JWT_SECRET']
     )
 
     if valid:
