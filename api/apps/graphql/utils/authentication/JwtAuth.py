75f05332d4a4cf9ea9716be496c2bf36a8427a7c
Novanda Ahsan <novandaahsan1@gmail.com>
2021-10-25 17:51:06 +0700

    add permission class

---
+++ api/apps/graphql/utils/authentication/JwtAuth.py
@@ -0,0 +1,33 @@
+import typing
+import jwt
+
+from starlette.requests import Request
+from starlette.websockets import WebSocket
+from strawberry.permission import BasePermission
+from strawberry.types import Info
+
+from apps.graphql.utils import const
+
+
+def authenticate_header(request: Request):
+    auth = request.headers.get('Authorization')
+    token = auth.split()[1]
+
+    valid = jwt.decode(token, const.jwt_secret)
+
+    if valid:
+        return True
+    else:
+        return False
+
+
+class JwtAuth(BasePermission):
+    message = "User is not authenticated"
+
+    def has_permission(self, source: typing.Any, info: Info, **kwargs) -> bool:
+        request: typing.Union[Request, WebSocket] = info.context["request"]
+
+        if "Authorization" in request.headers:
+            return authenticate_header(request)
+
+        return False
