95008c822a3bc1cbe5e68b524ed40b6f06531dc3
Novanda Ahsan <novandaahsan1@gmail.com>
2021-10-25 07:02:36 +0700

    use uuid on user and product, reset migrations

--- api/apps/user/models.py
+++ api/apps/user/models.py
@@ -1,8 +1,12 @@
+import uuid
 from django.db import models
+from django.db.models.fields import UUIDField
 from django.utils.translation import gettext as _
 
 
 class User(models.Model):
+    id = UUIDField(primary_key=True, default=uuid.uuid4,
+                   editable=False, unique=True)
     display_name = models.CharField(max_length=100, blank=True, null=True)
     phone = models.CharField(_("phone number"), max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
