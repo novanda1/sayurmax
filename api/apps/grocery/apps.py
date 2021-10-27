af52d249d3e14706d408330fbd7a674eaca8b58b
Novanda Ahsan <novandaahsan1@gmail.com>
2021-10-27 14:54:13 +0700

    upgrade django

--- api/apps/grocery/apps.py
+++ api/apps/grocery/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class GroceryConfig(AppConfig):
-    name = 'grocery'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.grocery'
--- api/core/settings.py
+++ api/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # app
+    # 'apps',
     'apps.grocery',
     'apps.user',
     'apps.otp',
