61bc87751eec3211adc573bb97b9c0a27709b968
Novanda Ahsan <novandaahsan1@gmail.com>
2021-10-28 16:38:52 +0700

    this day and yesterday filter start and end in 23

--- api/apps/order/admin.py
+++ api/apps/order/admin.py
@@ -1,9 +1,43 @@
+import datetime
+
 from django.contrib import admin
 from django.utils.translation import gettext as _
 from django.contrib.admin import RelatedFieldListFilter
 
 from apps.order.models import Order, OrderDetail, Invoice
 from apps.graphql.schema.order import InvoiceStatusCode
+
+
+class DayFilter(admin.SimpleListFilter):
+    title = _("Time")
+    parameter_name = "time"
+
+    def lookups(self, request, model_admin):
+        day_min_1 = datetime.datetime.today() - datetime.timedelta(days=1)
+        day_min_2 = datetime.datetime.today() - datetime.timedelta(days=2)
+
+        this_day__start = day_min_1.replace(
+            hour=23, minute=1).strftime('%Y-%m-%d')
+
+        this_day__end = datetime.datetime.today().replace(
+            hour=23, minute=1).strftime('%Y-%m-%d')
+
+        yesterday__start = day_min_2.replace(
+            hour=23, minute=1).strftime('%Y-%m-%d')
+
+        yesterday__end = day_min_1.replace(
+            hour=23, minute=1).strftime('%Y-%m-%d')
+
+        this_day__value = [this_day__start, this_day__end]
+        yesterday__value = [yesterday__start, yesterday__end]
+
+        return (
+            ([this_day__start, this_day__end], 'This day'),
+            ([yesterday__start, yesterday__end], 'Yesterday'),
+        )
+
+    def queryset(self, request, queryset):
+        return queryset.filter(created_at__range=this_day__value)
 
 
 class InvoiceStatusFilter(admin.SimpleListFilter):
@@ -47,7 +81,7 @@ class OrderDetailInline(admin.StackedInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'order_status_code',)
     fields = ('order_status_code',)
-    list_filter = [InvoiceStatusFilter]
+    list_filter = [DayFilter, InvoiceStatusFilter, ]
     inlines = [
         InvoiceDetailInline,
         OrderDetailInline
