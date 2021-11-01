86c3e89a262cb864091892e081b8cb9b053330cd
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-01 15:42:15 +0700

    use cons in setting.py

--- api/core/settings.py
+++ api/core/settings.py
@@ -11,11 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
-import os
-
-load_dotenv(dotenv_path='./.env.dev')
-
+from apps.graphql.utils import const
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = const.django_secret_key
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
