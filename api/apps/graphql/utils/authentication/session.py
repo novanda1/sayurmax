4043c73f5f85bbf4b16a740c6dbeb4cb8a5720ce
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-01 16:37:50 +0700

    session permission

---
+++ api/apps/graphql/utils/authentication/session.py
@@ -0,0 +1,34 @@
+import typing
+import jwt
+
+from django.contrib.sessions.models import Session
+from starlette.requests import Request
+from starlette.websockets import WebSocket
+from strawberry.permission import BasePermission
+from strawberry.types import Info
+
+from apps.graphql.utils import const
+
+
+def authenticate_header(request: Request):
+    auth = request.headers.get('Authorization')
+    token = auth.split()[1]
+
+    valid = jwt.decode(token, const.jwt_secret, algorithms=["HS256"])
+
+    if valid:
+        return True
+    else:
+        return False
+
+
+class SessionAuth(BasePermission):
+    message = "User is not authenticated"
+
+    def has_permission(self, source: typing.Any, info: Info, **kwargs) -> bool:
+        request: typing.Union[Request, WebSocket] = info.context["request"]
+
+        if request.session['userid']:
+            return True
+
+        return False
