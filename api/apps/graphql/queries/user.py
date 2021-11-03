ce07144662274b950485e038b27a64f15063f0cf
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-03 15:58:25 +0700

    user query: add jwt permission

--- api/apps/graphql/queries/user.py
+++ api/apps/graphql/queries/user.py
@@ -1,22 +1,24 @@
 from apps.user.models import User
 from apps.graphql.utils import const
+from apps.graphql.utils.authentication.default import JwtAuth, get_phone_from_jwt
+
 
 import jwt
 import strawberry
 
 
 class UserQuery:
-    @strawberry.field
+    @strawberry.field(permission_classes=[JwtAuth])
     def get_users():
         users = User.objects.all()
         return users
 
-    @strawberry.field
+    @strawberry.field(permission_classes=[JwtAuth])
     def get_user(phone: str):
         user = User.objects.get(phone=phone)
         return user
 
-    @strawberry.field
+    @strawberry.field(permission_classes=[JwtAuth])
     def verify_jwt(token: str):
         valid = jwt.decode(
             token,
