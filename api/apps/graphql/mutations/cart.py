35d812ba9c08e5889140f142236564a2bb15fce6
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-08 10:11:24 +0700

    make edit cart product amount work

--- api/apps/graphql/mutations/cart.py
+++ api/apps/graphql/mutations/cart.py
@@ -7,37 +7,27 @@ from apps.user.models import User
 
 from apps.graphql.services.cart import CartServices
 
-from apps.graphql.utils.authentication.default import JwtAuth, get_phone_from_jwt
+from apps.graphql.utils.authentication.default import JwtAuth
 
 
 cart_services = CartServices()
 
 
 class CartMutation:
-
     @strawberry.mutation(permission_classes=[JwtAuth])
     def add_to_cart(self, info: Info, product_id: str, amount: int):
-        request: Request = info.context["request"]
-
-        try:
-            phone = get_phone_from_jwt(request=request)
-        except:
-            raise Exception("not authenticated")
-
         cart = cart_services.add_to_cart(
             phone=phone, productid=product_id, amount=amount)
 
         return cart
 
     @strawberry.mutation(permission_classes=[JwtAuth])
+    def edit_cart_product(product_id: str, amount: int):
+        product_edited = cart_services.edit_product_amount(amount)
+        return product_edited
+
+    @strawberry.mutation(permission_classes=[JwtAuth])
     def delete_cart_product(self, info: Info, id: uuid.UUID):
-        request: Request = info.context["request"]
-
-        try:
-            phone = get_phone_from_jwt(request=request)
-        except:
-            raise Exception("not authenticated")
-
         cart_deleted = cart_services.delete_cart_product(id)
 
         return cart_deleted
