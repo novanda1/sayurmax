30fdb8185b1de6362fc4d6ab490e61adb5e316ab
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-08 09:49:47 +0700

    change how order ordering work

--- api/apps/order/admin.py
+++ api/apps/order/admin.py
@@ -7,37 +7,42 @@ from django.contrib.admin import RelatedFieldListFilter
 from apps.order.models import Order, OrderDetail, Invoice
 from apps.graphql.schema.order import InvoiceStatusCode
 
+day_min_1 = datetime.datetime.today() - datetime.timedelta(days=1)
+day_min_2 = datetime.datetime.today() - datetime.timedelta(days=2)
+
+this_day__start = day_min_1.replace(
+    hour=23, minute=1).strftime('%Y-%m-%d')
+
+this_day__end = datetime.datetime.today().replace(
+    hour=23, minute=1).strftime('%Y-%m-%d')
+
+yesterday__start = day_min_2.replace(
+    hour=23, minute=1).strftime('%Y-%m-%d')
+
+yesterday__end = day_min_1.replace(
+    hour=23, minute=1).strftime('%Y-%m-%d')
+
+this_day__value = [this_day__start, this_day__end]
+yesterday__value = [yesterday__start, yesterday__end]
+
 
 class DayFilter(admin.SimpleListFilter):
     title = _("Time")
     parameter_name = "time"
 
     def lookups(self, request, model_admin):
-        day_min_1 = datetime.datetime.today() - datetime.timedelta(days=1)
-        day_min_2 = datetime.datetime.today() - datetime.timedelta(days=2)
-
-        this_day__start = day_min_1.replace(
-            hour=23, minute=1).strftime('%Y-%m-%d')
-
-        this_day__end = datetime.datetime.today().replace(
-            hour=23, minute=1).strftime('%Y-%m-%d')
-
-        yesterday__start = day_min_2.replace(
-            hour=23, minute=1).strftime('%Y-%m-%d')
-
-        yesterday__end = day_min_1.replace(
-            hour=23, minute=1).strftime('%Y-%m-%d')
-
-        this_day__value = [this_day__start, this_day__end]
-        yesterday__value = [yesterday__start, yesterday__end]
 
         return (
-            ([this_day__start, this_day__end], 'This day'),
-            ([yesterday__start, yesterday__end], 'Yesterday'),
+            (0, 'Yesterday'),
+            (1, 'This day'),
         )
 
     def queryset(self, request, queryset):
-        return queryset.filter(created_at__range=this_day__value)
+        if self.value() == 0: 
+            return queryset.filter(created_at__range=yesterday__value)
+        elif self.value() == 1:
+            return queryset.filter(created_at__range=this_day__value)
+        return
 
 
 class InvoiceStatusFilter(admin.SimpleListFilter):
@@ -86,3 +91,6 @@ class OrderAdmin(admin.ModelAdmin):
         InvoiceDetailInline,
         OrderDetailInline
     ]
+
+    def has_add_permission(self, request, obj=None):
+        return False
