8ff9eaf67ae4342120e2c075f81d5717cefc0574
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-12 09:09:59 +0700

    docs

--- api/gql/queries/hello.py
+++ api/gql/queries/hello.py
@@ -1,2 +1,11 @@
 def say_hello():
     return "hello from gql"
+
+
+"""possible different pattern
+    
+#QueryResolver = create_type(name="Query", fields=[say_hello ])
+# @strawberry.field(name="hello")
+# def say_hello() -> str:
+#     return "hello from gql"
+"""
