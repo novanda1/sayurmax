f0d7fef9acab0bdf76f8c94f8254f9a7afef578f
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-15 15:12:05 +0700

    didnt use storybook

--- telo/screens/TabOneScreen.tsx
+++ telo/screens/TabOneScreen.tsx
@@ -3,7 +3,6 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
-import Button from '../storybook/stories';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
@@ -11,7 +10,6 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Button>Hai</Button>
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
