a4669b6214cc384c049358b9f8c1b85940c4e781
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-16 15:27:05 +0700

    api - add user address in user admin

--- api/apps/user/admin.py
+++ api/apps/user/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from apps.user.models import User
+from apps.user.models import User, UserAddress
 from django.contrib.auth.models import User as UserOld
 from django.contrib.auth.models import Group
+
+
+class AddressInline(admin.StackedInline):
+    model = UserAddress
+    extra = 0
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'phone', 'display_name')
     fields = ('phone', 'display_name')
+    inlines = [AddressInline]
 
 
 admin.site.unregister(UserOld)
