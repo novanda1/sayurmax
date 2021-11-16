a738f2b9a6d94d2ba4efb9960b7f967ee2baee23
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-16 13:41:31 +0700

    timun - add dummy bearer auth

--- timun/src/apollo/http-link.ts
+++ timun/src/apollo/http-link.ts
@@ -3,5 +3,9 @@ import { HttpLink } from "@apollo/client/link/http";
 export function createHttpLink() {
     return new HttpLink({
         uri: "http://localhost:9090/graphql/",
+        headers: {
+            authorization:
+                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxNDUwZDNiYS1iZmZhLTRlMDgtYWU3NC05OTZiN2NjNWU2YTEiLCJwaG9uZSI6Iis2Mjg1MTU1MTE0NzQ1IiwiZXhwIjoxNjM3MzA3MTQ4fQ.x52lrBxqd1JKbUI3WC5Ld4As88EaUD0QrRG0akDXlZk",
+        },
     });
 }
