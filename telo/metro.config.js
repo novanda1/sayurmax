4c382fd1c22523de55d51ff423cf796f075ec29e
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-16 21:13:38 +0700

    update stuff

--- telo/metro.config.js
+++ telo/metro.config.js
@@ -1,11 +1,18 @@
 const { createMetroConfiguration } = require("expo-yarn-workspaces");
+const path = require("path");
 
 const y = createMetroConfiguration(__dirname);
 
+const watchFolders = [
+    path.resolve(__dirname, "..", "node_modules"),
+    path.resolve(path.join(__dirname, "/../timun/")),
+];
+
 module.exports = {
-  ...y,
-  resolver: {
-    ...y.resolver,
-    sourceExts: ["jsx", "js", "ts", "tsx"],
-  },
+    ...y,
+    resolver: {
+        ...y.resolver,
+        sourceExts: ["jsx", "js", "ts", "tsx"],
+    },
+    watchFolders,
 };
