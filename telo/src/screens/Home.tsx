c1b8df8a32fd9e7a3712e554f53002e4b75fc00c
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-16 11:12:34 +0700

    telo - fetch worked (hello query tested)

--- telo/src/screens/Home.tsx
+++ telo/src/screens/Home.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { useHelloQuery } from "@sayurmax/timun";
 
 interface Props {}
 
 export const HomeScreen = (props: Props) => {
+  const { data } = useHelloQuery();
+
   return (
     <div>
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Home screen</Text>
+        <Text>Home screen {data?.hello}</Text>
       </View>
     </div>
   );
