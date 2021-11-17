db01f3728ccd44ca94cebaf20bf184364249f631
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-17 16:22:36 +0700

    telo - by this commmit expo all happy with the code:

--- telo/App.tsx
+++ telo/App.tsx
@@ -1,30 +1,33 @@
 import "react-native-gesture-handler";
 
-import { NavigationContainer } from "@react-navigation/native";
+// import { NavigationContainer } from "@react-navigation/native";
 import { extendTheme, NativeBaseProvider } from "native-base";
 import React from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+// import { SafeAreaProvider } from "react-native-safe-area-context";
 import { config } from "./src/config/theme";
-import { MainNavigation } from "./src/navigations/MainNavigation";
+// import { MainNavigation } from "./src/navigations/MainNavigation";
 import { ApolloProvider } from "@apollo/client";
-import { createApolloClient } from "@sayurmax/timun/src/apollo-client";
-import FontProvider from "./src/providers/FontProvider";
+import { SafeAreaView, Text } from "react-native";
+// import { createApolloClient } from "@sayurmax/timun/src/apollo-client";
+//import FontProvider from "./src/providers/FontProvider";
 
 export const theme = extendTheme({ config });
-const client = createApolloClient();
+// const client = createApolloClient();
 
 export default function App() {
   return (
-    <FontProvider>
-      <NativeBaseProvider theme={theme}>
-        <ApolloProvider client={client}>
-          <SafeAreaProvider>
+    <NativeBaseProvider theme={theme}>
+      {/*<ApolloProvider client={client}>
+        
+              <SafeAreaProvider>
             <NavigationContainer>
               <MainNavigation />
             </NavigationContainer>
           </SafeAreaProvider>
-        </ApolloProvider>
-      </NativeBaseProvider>
-    </FontProvider>
+          </ApolloProvider> */}
+      <SafeAreaView>
+        <Text> just hai </Text>
+      </SafeAreaView>
+    </NativeBaseProvider>
   );
 }
