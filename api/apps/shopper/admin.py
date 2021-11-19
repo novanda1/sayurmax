4f3e4d611965f7e19655a8f61ce5de3a8763d972
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-19 13:20:17 +0700

    api: we actually didnt need shopper model

--- api/apps/shopper/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from apps.shopper.models import Shopper
-
-
-@admin.register(Shopper)
-class ShopperAdmin(admin.ModelAdmin):
-    list_display = ('phone', 'display_name')
-    fields = ('phone', 'display_name', 'password')
