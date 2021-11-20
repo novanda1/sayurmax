8653830acd0bd8a63d58f6d86876fa4ee8bcab19
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-20 20:54:30 +0700

    add icons

---
+++ packages/shared/lib/icons/HomeIcon.d.ts
@@ -0,0 +1,5 @@
+import React from "react";
+export declare const HomeIcon: React.FC<{
+    fill?: string;
+    stroke?: string;
+}>;
---
+++ packages/shared/lib/icons/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HomeIcon = void 0;
+var HomeIcon_1 = require("./HomeIcon");
+Object.defineProperty(exports, "HomeIcon", { enumerable: true, get: function () { return HomeIcon_1.HomeIcon; } });
