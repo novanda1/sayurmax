24c94a22e5b3a9e750342220936d487db30bbfb6
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-22 10:47:03 +0700

    api: simplestupid order factories

---
+++ api/core/tests/factories/order.py
@@ -0,0 +1,29 @@
+import factory
+from faker import Faker
+import random
+
+from apps.grocery.models import Cart, CartProduct
+from apps.order.models import Order, OrderItem
+from apps.user.models import UserAddress
+
+
+def create():
+    carts = Cart.objects.all()
+
+    for c in carts:
+        address = UserAddress.objects.filter(user=c.user)
+
+        order = Order(
+            user=c.user, order_status_code=0,
+            address=address[random.randrange(len(address))],
+            invoice_status_code=0)
+        order.save()
+
+        items = CartProduct.objects.filter(cart=c)
+
+        for item in items:
+            order_item = OrderItem(
+                order=order,
+                product=item.product,
+                qty=random.randint(1, 5))
+            order_item.save()
--- api/core/tests/factories/product.py
+++ api/core/tests/factories/product.py
@@ -1,5 +1,3 @@
-from django.db import models
-from django.template.defaultfilters import title
 import factory
 from faker import Faker
 import apps
