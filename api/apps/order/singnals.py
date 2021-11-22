c72cca418ad4234c65d602970583d78a0c448fd9
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-22 10:29:33 +0700

    api: orderitem signals on delte

--- api/apps/order/singnals.py
+++ api/apps/order/singnals.py
@@ -1,11 +1,23 @@
 from apps.order.models import OrderItem
 
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 
 @receiver(post_save, sender=OrderItem)
-def change_order_total(sender, instance, created, **kwargs):
+def save_order_total(sender, instance, created, **kwargs):
+    order = instance.order
+    items = OrderItem.objects.filter(order=order)
+
+    total = 0
+    for item in items:
+        total += item.at_price
+
+    order.save()
+
+
+@receiver(post_delete, sender=OrderItem)
+def delete_order_total(sender, instance, using, **kwargs):
     order = instance.order
     items = OrderItem.objects.filter(order=order)
 
