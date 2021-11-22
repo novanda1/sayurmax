03e9196f1c9a05fb249a819c32c99ff60f584e89
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-22 09:30:53 +0700

    api: factories - simple cart factory

--- api/core/tests/factories/cart.py
+++ api/core/tests/factories/cart.py
@@ -20,17 +20,18 @@ class CartProductFactory(factory.django.DjangoModelFactory):
     cart = apps.grocery.models.Cart
 
 
-def create(l: int):
+def create():
     users = list(apps.grocery.models.User.objects.all())
     products = list(apps.grocery.models.Product.objects.all())
 
-    for i in users:
-        random_user = random.choice(users)
-        random_product = random.choice(products)
+    for user in users:
 
-        cart = apps.grocery.models.Cart(user=random_user)
+        cart = apps.grocery.models.Cart(user=user)
         cart.save()
 
-        CartProductFactory(
-            product=random_product,
-            amount=random.randrange(10), cart=cart)
+        for i in range(random.randint(1, 10)):
+
+            CartProductFactory(
+                product=products[i],
+                amount=random.randint(1, 10), cart=cart
+            )
