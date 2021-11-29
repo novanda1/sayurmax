69dad92267c7e34c51d6725f0337ea1bdfe07afb
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-29 14:01:43 +0700

    order: simplified order screen & make search worked

--- packages/telo/src/components/order/OrderList.tsx
+++ packages/telo/src/components/order/OrderList.tsx
@@ -14,55 +14,14 @@ import IonIcon from "react-native-vector-icons/Ionicons";
 
 interface Props {
     children: React.ReactNode;
+    topELement?: React.ReactNode;
 }
 
-export const OrderList: React.FC<Props> = ({ children }) => {
+export const OrderList: React.FC<Props> = ({ children, topELement }) => {
     return (
         <>
             <Container minH={100}>
-                <Heading size="md" mt="6">
-                    32 Pesanan Belum diantar
-                </Heading>
-                <Text>Kamu punya pesanan yang harus diantar hari ini</Text>
-
-                <HStack space="2" mt="3">
-                    <Input
-                        placeholder="Cari list"
-                        bg="white"
-                        flex="1"
-                        borderRadius="4"
-                        py="3"
-                        px="1"
-                        fontSize="14"
-                        _web={{
-                            _focus: {
-                                borderColor: "muted.300",
-                                style: {},
-                            },
-                        }}
-                        InputLeftElement={
-                            <Icon
-                                m="2"
-                                ml="3"
-                                size="6"
-                                color="gray"
-                                as={<Feather name="search" />}
-                            />
-                        }
-                    />
-                    <IconButton
-                        variant="solid"
-                        backgroundColor="white"
-                        my="1px"
-                        w="45px"
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="center"
-                        icon={<IonIcon name="filter-sharp" />}
-                        _icon={{ fontSize: "20px" }}
-                    />
-                </HStack>
-
+                {topELement}
                 <VStack space="2" alignItems="center" w="100%" mt="4">
                     {children}
                 </VStack>
---
+++ packages/telo/src/modules/order/useOrder.tsx
@@ -0,0 +1,74 @@
+import { Icon, Input } from "native-base";
+import Feather from "react-native-vector-icons/Feather";
+
+import React, { useState, useCallback, useEffect } from "react";
+import { useOrderStore } from "./useOrderStore";
+import { OrdersQuery, OrderStatusCode, useOrdersQuery } from "@sayurmax/shared";
+import { UseQueryState } from "urql";
+
+interface Props {
+    orderStatus: OrderStatusCode;
+}
+
+interface Return {
+    SearchInput: React.ReactNode;
+    result: UseQueryState<OrdersQuery, object>;
+}
+
+export const useOrder = ({ orderStatus }: Props): Return => {
+    const { date } = useOrderStore();
+    const [search, setSearch] = useState<string>("");
+
+    const [result, reexecuteQuery] = useOrdersQuery({
+        variables: {
+            status: orderStatus,
+            limit: 11,
+            date: {
+                year: date.year(),
+                month: date.month() + 1,
+                day: date.date(),
+            },
+            search,
+        },
+    });
+
+    const handleInputSearch = useCallback(
+        (e) => {
+            setSearch(e.target.value);
+            reexecuteQuery();
+        },
+        [setSearch]
+    );
+
+    useEffect(() => {
+        reexecuteQuery();
+    }, [date]);
+
+    return {
+        result,
+        SearchInput: (
+            <>
+                <Input
+                    placeholder="Cari list"
+                    bg="white"
+                    flex="1"
+                    borderRadius="4"
+                    py="3"
+                    px="1"
+                    fontSize="14"
+                    value={search}
+                    onChange={handleInputSearch}
+                    InputLeftElement={
+                        <Icon
+                            m="2"
+                            ml="3"
+                            size="6"
+                            color="gray"
+                            as={<Feather name="search" />}
+                        />
+                    }
+                />
+            </>
+        ),
+    };
+};
