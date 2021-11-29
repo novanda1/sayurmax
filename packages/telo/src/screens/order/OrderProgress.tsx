1d6cfc8e564c757fe687d19c6885ca2e4b3561ea
Novanda Ahsan <novandaahsan1@gmail.com>
2021-11-29 11:38:05 +0700

    telo: make orders filter by date worked

--- packages/telo/src/screens/order/OrderProgress.tsx
+++ packages/telo/src/screens/order/OrderProgress.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { OrderItem } from "../../components/order/OrderItem";
 import { OrderList } from "../../components/order/OrderList";
 import { OrderStatusCode, useOrdersQuery } from "@sayurmax/shared";
 import { ScrollView, Box, Pressable } from "native-base";
+import { useOrderStore } from "../../modules/order/useOrderStore";
 
 export const OrderProgressScreen = ({ navigation }: any) => {
-    const [result] = useOrdersQuery({
+    const { date } = useOrderStore();
+
+    const [result, reexecuteQuery] = useOrdersQuery({
         variables: {
             status: OrderStatusCode.Progress,
-            limit: 10
+            limit: 10,
+            date: {
+                year: date.year(),
+                month: date.month() + 1,
+                day: date.date(),
+            },
         },
     });
 
     const { data } = result;
+
+    useEffect(() => {
+        reexecuteQuery();
+    }, [date]);
+
     return (
         <ScrollView>
             <OrderList>
--- packages/telo/src/screens/order/OrderUnverif.tsx
+++ packages/telo/src/screens/order/OrderUnverif.tsx
@@ -1,17 +1,29 @@
 import { OrderStatusCode, useOrdersQuery } from "@sayurmax/shared";
 import { Box, Pressable, ScrollView } from "native-base";
-import React from "react";
+import React, { useEffect } from "react";
 import { OrderItem } from "../../components/order/OrderItem";
 import { OrderList } from "../../components/order/OrderList";
+import { useOrderStore } from "../../modules/order/useOrderStore";
 
 export const OrderUnverifScreen: React.FC = ({ navigation }: any) => {
-    const [result] = useOrdersQuery({
+    const { date } = useOrderStore();
+
+    const [result, reexecuteQuery] = useOrdersQuery({
         variables: {
             status: OrderStatusCode.Unverified,
-            limit: 10
+            limit: 11,
+            date: {
+                year: date.year(),
+                month: date.month() + 1,
+                day: date.date(),
+            },
         },
     });
     const { data } = result;
+
+    useEffect(() => {
+        reexecuteQuery();
+    }, [date]);
 
     return (
         <ScrollView>
