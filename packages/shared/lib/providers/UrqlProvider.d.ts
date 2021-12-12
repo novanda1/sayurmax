053fbec3ea62bb17768119d49d0d545994ff2588
Novanda Ahsan <novandaahsan1@gmail.com>
2021-12-12 12:33:32 +0700

    with rn init

--- packages/shared/lib/providers/UrqlProvider.d.ts
+++
@@ -1,6 +0,0 @@
-import React, { ReactElement } from "react";
-interface Props {
-    children: React.ReactNode;
-}
-declare function UrqlProvider({ children }: Props): ReactElement;
-export default UrqlProvider;
--- packages/shared/src/providers/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as UrqlProvider } from "./UrqlProvider";
